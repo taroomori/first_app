cd014f3a0b7ae3f8c477030d16f01344d1a86ae7
nakamura kotaro <fallout.taro@gmail.com>
2013-01-16 23:28:07 +0900

    Initial commift

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = '8a9f5f01f8ad40345503a6e4851363919acd26b76a7585509f2d41078b9f58ee4a10ec798a3c96a14b000bf5b52f97ef023bfce32b2d49e893af9c7e5b59d636'
